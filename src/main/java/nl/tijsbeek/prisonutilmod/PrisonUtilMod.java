4437052c3bc7ed95612cef828c9202686d76e1d4
Tais993 <tijs@familiebeek.eu>
2022-07-12 20:39:41 +0200

    Added timer for mining
    This timer can be reset using the `!reset-mine` command
    The timer lasts for 5 minutes, and resets automatically.

--- src/main/java/nl/tijsbeek/prisonutilmod/PrisonUtilMod.java
+++ src/main/java/nl/tijsbeek/prisonutilmod/PrisonUtilMod.java
@@ -9,6 +9,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import nl.tijsbeek.prisonutilmod.minereset.MineTimer;
 import org.slf4j.Logger;
 
 @Mod("prisonutilmod")
@@ -22,6 +23,7 @@ public class PrisonUtilMod {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
 
         MinecraftForge.EVENT_BUS.register(this);
+        MinecraftForge.EVENT_BUS.register(new MineTimer());
     }
 
     private void setup(final FMLCommonSetupEvent event) {
---
+++ src/main/java/nl/tijsbeek/prisonutilmod/minereset/MineTimer.java
@@ -0,0 +1,67 @@
+package nl.tijsbeek.prisonutilmod.minereset;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.client.event.ClientChatEvent;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+import java.awt.*;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+public class MineTimer {
+
+    private int minute;
+    private int seconds;
+
+    private final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+
+    private ScheduledFuture<?> timer;
+
+    public MineTimer() {
+        resetTimer();
+    }
+
+    public void resetTimer() {
+        seconds = 0;
+        minute = 0;
+
+        timer = executorService.scheduleAtFixedRate(() -> {
+            seconds++;
+
+            if (seconds == 60) {
+                minute++;
+                seconds = 0;
+
+                if (minute == 5) {
+                    executorService.schedule(this::resetTimer, 1, TimeUnit.SECONDS);
+                    timer.cancel(false);
+                }
+            }
+        }, 0, 1, TimeUnit.SECONDS);
+    }
+
+    @SubscribeEvent
+    public void onRenderGui(RenderGameOverlayEvent event) {
+        if (!event.isCancelable() && event.getType() == RenderGameOverlayEvent.ElementType.TEXT) {
+            String format = "%02d:%02d".formatted(minute, seconds);
+
+            Minecraft.getInstance().font.drawShadow(new PoseStack(), format, 0, 0, Color.ORANGE.getRGB(), true);
+        }
+    }
+
+    @SubscribeEvent
+    public void onClientChatEvent(ClientChatEvent event) {
+        String message = event.getMessage();
+
+        if (message.equals("!reset-mine")) {
+            timer.cancel(true);
+            resetTimer();
+
+            event.setCanceled(true);
+        }
+    }
+}
