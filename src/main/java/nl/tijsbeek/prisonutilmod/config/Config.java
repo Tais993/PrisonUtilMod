eb274da9583269f92a8b9e094be6bebac33bda88
Tais993 <tijs@familiebeek.eu>
2022-11-25 11:42:50 +0100

    Added persistent mine reset times
    Restarting your client will now result in the previously defined mine reset time to stay.
    
    Added: 'MINE_RESET_COOLDOWN' and 'LAST_TIME_MINE_RESET' values
    Mine reset cooldown contains how long it takes for the mine to be reset, currently 5 minutes.
    
    Last time mine reset is an UNIX timestamp
    
    MineTimer now automatically resets the time to be consistent with the 'LAST_TIME_MINE_RESET' config value every 10 seconds.

--- src/main/java/nl/tijsbeek/prisonutilmod/config/Config.java
+++ src/main/java/nl/tijsbeek/prisonutilmod/config/Config.java
@@ -1,20 +1,35 @@
 package nl.tijsbeek.prisonutilmod.config;
 
 import net.minecraftforge.common.ForgeConfigSpec;
+import org.jetbrains.annotations.Contract;
+
+import java.time.Instant;
 
 public class Config {
     public static final ForgeConfigSpec GENERAL_SPEC;
 
-    public static ForgeConfigSpec.ConfigValue<String> ITEMS_PATH;
+    public static final ForgeConfigSpec.ConfigValue<String> ITEMS_PATH;
+    public static final ForgeConfigSpec.ConfigValue<Long> MINE_RESET_COOLDOWN;
+    public static final ForgeConfigSpec.ConfigValue<Long> LAST_TIME_MINE_RESET;
 
     static {
-        ForgeConfigSpec.Builder configBuilder = new ForgeConfigSpec.Builder();
-        setupConfig(configBuilder);
-        GENERAL_SPEC = configBuilder.build();
+        ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
+
+        ITEMS_PATH  = builder.define("items_path", "");
+
+        builder.comment("Seconds it takes for the mine to reset");
+        MINE_RESET_COOLDOWN = builder.define("mine_reset_cooldown", 300L);
+
+        builder.comment("The following items should most likely be ignored, these are automatically modified by the mod.");
+
+        builder.comment("Unix timestamp");
+        LAST_TIME_MINE_RESET = builder.define("last_time_mine_reset", Instant.now().toEpochMilli());
+
+        GENERAL_SPEC = builder.build();
     }
 
-    private static void setupConfig(ForgeConfigSpec.Builder builder) {
-        builder.comment("The path of your items should be stored here");
-        ITEMS_PATH  = builder.define("items_path", "");
+    @Contract(value = " -> fail", pure = true)
+    private Config() {
+        throw new IllegalStateException("Class shouldn't be initiated!");
     }
 }
--- src/main/java/nl/tijsbeek/prisonutilmod/minereset/MineTimer.java
+++ src/main/java/nl/tijsbeek/prisonutilmod/minereset/MineTimer.java
@@ -5,80 +5,89 @@ import net.minecraft.client.Minecraft;
 import net.minecraftforge.client.event.ClientChatEvent;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import nl.tijsbeek.prisonutilmod.config.Config;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.awt.*;
+import java.awt.Color;
+import java.time.Instant;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
-import java.util.function.Consumer;
 
-public class MineTimer {
+public final class MineTimer {
+    private static final Logger logger = LoggerFactory.getLogger(MineTimer.class);
 
-    private int minute;
-    private int seconds;
+    private static final long SECONDS_IN_MINUTE = 60L;
 
-    private final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+    private long seconds = 0L;
 
-    private ScheduledFuture<?> timer;
-
-    private final Lock lock = new ReentrantLock();
 
     public MineTimer() {
-        resetTimer();
+        ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+
+        executorService.scheduleAtFixedRate(() -> {
+            seconds++;
+            logger.warn("Seconds: {}", seconds);
+
+            if (seconds == Config.MINE_RESET_COOLDOWN.get()) {
+                resetTimeTo(0);
+            }
+
+        }, 0, 1, TimeUnit.SECONDS);
+
+        executorService.scheduleAtFixedRate(() -> {
+            resetTimerFromInstant(Instant.ofEpochMilli(Config.LAST_TIME_MINE_RESET.get()));
+        }, 0, 10, TimeUnit.SECONDS);
     }
 
 
-    public void accessTimer(Consumer<ScheduledFuture<?>> timerUser) {
-        lock.lock();
-        try {
-            timerUser.accept(timer);
-        } finally {
-            lock.unlock();
+    /**
+     * Resets the time to the given seconds.
+     * <p>
+     * The timer resets automatically once it surpasses the cooldown assigned in the config.
+     *
+     * @param resetToSeconds the
+     */
+    public void resetTimeTo(final long resetToSeconds) {
+        if (resetToSeconds > Config.MINE_RESET_COOLDOWN.get()) {
+            throw new IllegalArgumentException("The time cannot be above the cooldown");
         }
-    }
 
-    public void setTimer(ScheduledFuture<?> timer) {
-        lock.lock();
-        try {
-            this.timer = timer;
-        } finally {
-            lock.unlock();
-        }
+        logger.info("Resetted the time to {}", resetToSeconds);
+        seconds = resetToSeconds;
     }
 
     /**
-     * Restarts the timer at 1.
+     * Resets the time based on the given instant.
      * <p>
-     * This gets recursively called by the timer itself.
-     * <p>
-     * Before resetting, one should make sure the existing {@link #timer timer} is cancelled.
+     * The timer resets automatically once it surpasses the cooldown assigned in the config.
      */
-    public void resetTimer() {
-        seconds = 0;
-        minute = 0;
+    public void resetTimerFromInstant(final Instant instant) {
+        long difference = Instant.now().getEpochSecond() - instant.getEpochSecond();
 
-        setTimer(executorService.scheduleAtFixedRate(() -> {
-            seconds++;
+        long mineResetCooldown = Config.MINE_RESET_COOLDOWN.get();
 
-            if (seconds == 60) {
-                minute++;
-                seconds = 0;
+        while (difference >= mineResetCooldown) {
+            difference -= mineResetCooldown;
+        }
 
-                if (minute == 5) {
-                    executorService.schedule(this::resetTimer, 1, TimeUnit.SECONDS);
-                    accessTimer(timer -> timer.cancel(true));
-                }
-            }
-        }, 0, 1, TimeUnit.SECONDS));
+        resetTimeTo(difference);
     }
+
 
     @SubscribeEvent
     public void onRenderGui(RenderGameOverlayEvent event) {
-        if (!event.isCancelable() && event.getType() == RenderGameOverlayEvent.ElementType.TEXT) {
-            String format = "%02d:%02d".formatted(minute, seconds);
+        if (!event.isCancelable() && RenderGameOverlayEvent.ElementType.TEXT == event.getType()) {
+
+            int displayMinutes = 0;
+            long displaySeconds = seconds;
+            while (SECONDS_IN_MINUTE <= displaySeconds) {
+                displayMinutes++;
+                displaySeconds -= SECONDS_IN_MINUTE;
+            }
+
+            String format = "%02d:%02d".formatted(displayMinutes, displaySeconds);
 
             Minecraft.getInstance().font.drawShadow(new PoseStack(), format, 0, 0, Color.ORANGE.getRGB(), true);
         }
@@ -89,8 +98,8 @@ public class MineTimer {
         String message = event.getMessage();
 
         if (message.equals("!reset-mine")) {
-            accessTimer(timer -> timer.cancel(true));
-            resetTimer();
+            resetTimeTo(0);
+            Config.LAST_TIME_MINE_RESET.set(Instant.now().toEpochMilli());
 
             event.setCanceled(true);
         }
